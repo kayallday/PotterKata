111e98aca426188d14bca97f871e4ac758ba6cf7
Kelsey Beard <kbeard@apterainc.com>
2017-05-22 16:25:08 -0400

    Testing to see if Potter Pricer exists

---
+++ PotterKataTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PotterKataTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PotterKataTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3f3f9bb-72fd-4549-ab92-5213918137ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PotterKataTests/ThePotterPricer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterKata;
+
+namespace PotterKataTests
+{
+	[TestClass]
+	public class ThePotterPricer
+	{
+		[TestMethod]
+		public void Exists()
+		{
+			var pricer = new PotterPricer();
+		}
+
+		[TestMethod]
+		public void BookPrices()
+		{
+			var pricer = new PotterPricer();
+			
+		}
+
+	}
+}
